e8c1dea093c6fa13e2bb661b618c38ba3b276587
gabrielsinca <gabrielsinca14@gmail.com>
2018-12-26 21:51:55 +0200

    finished the reveal on scroll property

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
-var mobileMenu = new MobileMenu;
+var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
